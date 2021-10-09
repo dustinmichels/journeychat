7ab0d21ca6ce2460bf196e58ca528235dd314c84
Dustin Michels <dustin7538@gmail.com>
2021-10-08 21:50:54 -0600

    new websocket functionality

--- journeychat/socketio/app.py
+++ journeychat/socketio/app.py
@@ -1,3 +1,4 @@
+from pydantic.errors import UrlHostError
 import socketio
 from socketio.exceptions import ConnectionRefusedError
 from sqlalchemy.orm.session import Session
@@ -17,23 +18,20 @@ if settings.BACKEND_CORS_ORIGINS:
 sio = socketio.AsyncServer(
     async_mode="asgi",
     cors_allowed_origins=origins,
-    # logger=True,
-    # engineio_logger=True,
 )
 ws_app = socketio.ASGIApp(sio)
 
 CONNECTIONS = {}
 
 
-def refresh_rooms(user, sid):
+def refresh_rooms(user: User, sid: str):
     for room in user.joined_rooms:
-        # print(f"Adding to room {room.id} ({room.name})")
         sio.enter_room(sid, room.id)
 
 
 @sio.event
 async def connect(sid, environ, auth):
-    print("> connect ", sid)
+    print("> WS: connect ", sid)
     # will return authenticated user or raise error
     user = await get_authenticated_user(auth["token"])
 
@@ -44,31 +42,13 @@ async def connect(sid, environ, auth):
     refresh_rooms(user, sid)
     print(f"User {user.username} is in rooms: {sio.rooms(sid)}")
 
-    for room in sio.rooms(sid):
-        await sio.emit("online-ping", data=user.username, room=room, skip_sid=sid)
-
-    # session = await sio.get_session(sid)
-    # CONNECTIONS.append(session)
-    # authenticate user
-    # user = authenticate(auth["token"])
-    # await sio.save_session(sid, {"username": user["username"]})
-    # refresh_rooms(user, sid)
-
 
 @sio.on("new-message")
 async def recieve_chat(sid, data):
-    print("message:", data)
+    print("> WS: new-message:", data)
 
     # parse as Pydantic object
     msg = schemas.MessageCreate(**data)
-
-    # get username from session
-    # session = await sio.get_session(sid)
-    # data["username"] = session["username"]
-
-    # if msg.room_id not in sio.rooms(sid):
-    #     print("No access!")
-    #     return
 
     # Add message to database
     db: Session = next(deps.get_db())
@@ -77,8 +57,22 @@ async def recieve_chat(sid, data):
     await sio.emit("new-message", data, room=int(data["room_id"]))
 
 
+@sio.on("join-room")
+async def join_room(sid, data):
+    print("> WS: join-room")
+    room = data["room"]
+    sio.enter_room(sid, room["id"])
+    await sio.emit("new-member", data)
+
+
+@sio.on("leave-room")
+def leave_room(sid, room_id):
+    print("> WS: leave-room")
+    sio.leave_room(sid, room_id)
+
+
 @sio.event
 def disconnect(sid):
-    print("disconnect ", sid)
+    print("> WS disconnect", sid)
     del CONNECTIONS[sid]
     print(CONNECTIONS)
